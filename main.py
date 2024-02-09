0c034c2e68f08ae49f4ff0cc89e09dbf33f4364b
Cade Duncan <42172655+Stingrayss@users.noreply.github.com>
2024-02-08 19:00:00 -0800

    Added main() (#12)

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-print("Hello World!")
-print ("I like your dog.")
-print("Test")
-print("pp caca")
-print("lol this class is great")
+def main():
+    print("Hello World!")
+    print("I like your dog.")
+    print("Test")
+    print("pp caca")
+    print("lol this class is great")
+    
+if __name__ == '__main__':
+    main()
